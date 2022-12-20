bdc95b906ed60f6bf543d4113b218ad08a90c233
atinda <atindasamson@gmail.com>
2022-12-20 21:14:43 +0300

    hello

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#bin/bash
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 print(str[39:66] + str[106:112] + str[:6])
